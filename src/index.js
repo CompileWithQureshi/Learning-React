5e60f787a036e04aa4f6f8a3ee78080ada6bc899
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-11-04 04:40:47 -0700

    Learning components,render method,state,className and Refactor method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from "./components/counter.jsx"
 
-const element = <h1>Hello world </h1>
-ReactDOM.render(element, document.getElementById('root'))
-console.log(element);
+ReactDOM.render(<Counter />, document.getElementById('root'))
