e48cfcd0bdb363fb91faec2126ffc70b89dc29f7
CompileWithQureshi <99005823+CompileWithQureshi@users.noreply.github.com>
2022-11-07 08:13:14 -0800

    Learning state and event handling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,32 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state={
-        count: 0,
-        tags:['tag1','tag2','tag3']
-    };
-    render() {
-        return(
-            <div>
-                
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <input type="button" value="Incerement" className='btn btn-secondary btn-sm'/>
-                <ul>
-                    {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
-                </ul>
-            </div>
-        );
-    }
-    getBadgeClasses() {
-        let classes = "badge m-2 p-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
 
-    formatCount(){
-        const {count}=this.state;
-        return count === 0 ? "zero" : count;
-    }
+  handelInceremnet = (params) => {
+    console.log(params);
+    this.setState({ count: this.state.count + 1 });
+  };
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <input
+          type="button"
+          value="Incerement"
+          className="btn btn-secondary btn-sm"
+          onClick={() => this.handelInceremnet()}
+        />
+        {/* <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li> //Here we are using map object method to add li tags in this ul by render method in react.
+          ))}
+        </ul> */}
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 p-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
+  }
 }
- 
+
 export default Counter;
